487c052a9e7cd73f7ec3e7bdddb780a9e61296da
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-13 18:38:40 +0700

    added email functionality

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const claimBtn = document.querySelector('.claim-trial-btn');
-const firstNameInput = document.querySelector('.first-name');
-const lastNameInput = document.querySelector('.last-name');
-const emailAddress = document.querySelector('.email-address');
-const passwordInput = document.querySelector('.password');
-const firstNameError = document.getElementById('first-name-error');
-const lastNameError = document.getElementById('last-name-error');
-const passwordError = document.getElementById('password-error');
 
+const firstNameInput = document.querySelector('.first-name');
+const firstNameError = document.getElementById('first-name-error');
+
+const lastNameInput = document.querySelector('.last-name');
+const lastNameError = document.getElementById('last-name-error');
+
+let emailError = document.getElementById('email-error');
 let email = document.getElementById('email-address');
+
+const passwordInput = document.querySelector('.password');
+const passwordError = document.getElementById('password-error');
 
 claimBtn.addEventListener('click', submitInfo);
 
@@ -33,5 +36,15 @@ function submitInfo(event) {
     passwordError.classList.add('errorMsg');
     passwordInput.removeAttribute('placeholder');
     passwordInput.classList.add('emptyInput', 'icon-error');
+  }
+
+  let regex = /^\S+@\S+\.\S+$/;
+  if (regex.test(email.value) === true) {
+    emailError = '';
+  } else {
+    emailError.textContent = 'Looks like this is not an email';
+    emailError.classList.add('errorMsg');
+    email.removeAttribute('placeholder');
+    email.classList.add('emptyInput', 'icon-error');
   }
 }
