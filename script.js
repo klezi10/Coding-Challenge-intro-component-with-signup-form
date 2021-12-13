108f1c7321cb0f1cd59d049d976ca852cdd28f2e
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-13 16:53:22 +0700

    functionality for error msgs (first and last name)

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let claimBtn = document.querySelector('.claim-trial-btn');
+const claimBtn = document.querySelector('.claim-trial-btn');
+const firstNameInput = document.querySelector('.first-name');
+const lastNameInput = document.querySelector('.last-name');
+const emailAddress = document.querySelector('.email-address');
+const password = document.querySelector('.password');
+const firstNameError = document.getElementById('first-name-error');
+const lastNameError = document.getElementById('last-name-error');
+
 claimBtn.addEventListener('click', submitInfo);
 
 function submitInfo(event) {
   event.preventDefault();
-  console.log('clicked');
+
+  if (firstNameInput.value === '') {
+    firstNameError.textContent = `First Name cannot be empty`;
+    firstNameError.classList.add('errorMsg');
+    firstNameInput.removeAttribute('placeholder');
+    firstNameInput.classList.add('emptyInput', 'icon-error');
+  } else {
+    return null;
+  }
+
+  if (lastNameInput.value === '') {
+    lastNameError.textContent = `Last Name cannot be empty`;
+    lastNameError.classList.add('errorMsg');
+    lastNameInput.removeAttribute('placeholder');
+    lastNameInput.classList.add('emptyInput', 'icon-error');
+  } else {
+    return null;
+  }
 }
